aab44b17ed9f32a6688f118fdeffc5823eb5afec
Valentin <zvezdochet.79@mail.ru>
2023-10-29 19:45:47 +0300

    dz_5

---
+++ Calc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_5
+{
+
+
+    internal class Calc : ICalc
+    {
+        public double Result { get; set; }
+        private Stack<double> LastResult { get; set; } = new Stack<double>();
+
+        public event EventHandler<EventArgs> MyEvent;
+
+            private void PrintEvent()
+            {
+                MyEvent?.Invoke(this, new EventArgs());
+            }
+
+            public void Div(double x)
+            {
+                Result /= x;
+                PrintEvent();
+                LastResult.Push(Result);
+            }
+
+            public void Mul(double x)
+            {
+                Result *= x;
+                PrintEvent();
+                LastResult.Push(Result);
+            }
+
+            public void Sub(double x)
+            {
+                Result -= x;
+                PrintEvent();
+                LastResult.Push(Result);
+            }
+
+            public void Sum(double x)
+            {
+                Result += x;
+                PrintEvent();
+                LastResult.Push(Result);
+            }
+
+            public void CancelLast()
+            {
+                if (LastResult.TryPop(out double res))
+                {
+                    Result = res;
+                    Console.WriteLine("Canceled");
+                    PrintEvent();
+                }
+                else
+                {
+                    Console.WriteLine("Невозможно отменить");
+                }
+            }    
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace CS_DZ_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calc();
+
+            calc.MyEvent += Calc_MyEvent;
+            
+            string action = "";
+            Console.WriteLine("Введите число: ");
+            calc.Result = Convert.ToDouble(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Выберете действие: +, -, *, /, q - для выхода");
+                action = Console.ReadLine();
+                if (action == "+" || action == "-" || action == "*" || action == "/")
+                {
+                    Console.WriteLine("Введите число: ");
+                    var num = Console.ReadLine();
+                    if (double.TryParse(num, out double n))
+                    {
+                        switch (action)
+                        {
+                            case "+": calc.Sum(n); continue;
+                            case "-": calc.Sub(n); continue;
+                            case "*": calc.Mul(n); continue;
+                            case "/": calc.Div(n); continue;                              
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Ошибка: Введено не число ({num})");
+                    }
+                }
+                else if(!(action == "q" || action == ""))
+                {
+                    Console.WriteLine("Некорректный оператор");
+                }
+
+            }
+            while (action != "q" && action != "");
+            Console.WriteLine("Работа завершена");
+
+        }
+
+
+        private static void Calc_MyEvent(object? sender, EventArgs e)
+        {
+            if (sender is Calc)
+                Console.WriteLine(((Calc)sender).Result);
+        }
+    }
+}
